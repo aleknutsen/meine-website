7a60e9ccb5fe1f99eea22c6ac752f9203265c1a0
aleknutsen <alexandreknutsen@hotmail.de>
2025-02-05 13:47:16 +0100

    Herz-Explosion hinzugefügt

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function zeigeNachricht() {
     alert("🚀 Du hast auf den Button geklickt!");
 }
+function zeigeHerzExplosion() {
+    const herz = document.createElement("div");
+    herz.innerHTML = "💖";
+    herz.style.position = "absolute";
+    herz.style.left = Math.random() * window.innerWidth + "px";
+    herz.style.top = Math.random() * window.innerHeight + "px";
+    herz.style.fontSize = "50px";
+    herz.style.opacity = "1";
+    herz.style.transition = "opacity 1s ease-out, transform 1s ease-out";
+    
+    document.body.appendChild(herz);
+    
+    setTimeout(() => {
+        herz.style.opacity = "0";
+        herz.style.transform = "scale(3)";
+    }, 100);
+    
+    setTimeout(() => {
+        herz.remove();
+    }, 2000);
+}
